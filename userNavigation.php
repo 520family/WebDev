bb1bfd4ab50ff06514bd710c38eb3903641d6f97
Terence <tankahchee00@gmail.com>
2021-02-08 19:16:57 +0800

    added homepage

--- userNavigation.php
+++ userNavigation.php
@@ -1,5 +1,6 @@
 <div class="topnav">
     <ul>
+        <li><a href="homepage.php?username=<?php echo $id; ?>" >Homepage</a></li>
         <li><a href="myProfile.php?username=<?php echo $id; ?>" >My Profile</a></li>
         <li><a href="myReservation.php?username=<?php echo $id; ?>" >My Reservation</a></li>
         <li><a href="RecitalConcert.php?username=<?php echo $id; ?>" >Recital</a></li>
